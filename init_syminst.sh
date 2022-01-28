8366901f68463c77d41620b5ab71638d848e209b
mgholami <16431410+gongotar@users.noreply.github.com>
2022-01-28 18:19:07 +0100

    edit author

---
+++ init_syminst.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# default values
+
+package_path=$HOME/.apps/packages
+install_path=$HOME/.apps/installs
+export_root=$HOME/.apps/root
+cores=`cat /proc/cpuinfo | grep processor | wc -l`
+installer=syminst.sh
+
+usage() {                                 # Function: Print a help message.
+    echo "Usage: $0 [ -p package_path ] [ -r export_root ] [ -i install_path ] [ -j cores ] [ -s installer_path ]" 1>&2 
+}
+exit_abnormal() {                         # Function: Exit with error.
+    echo "Error:" 1>&2
+    echo $1 1>&2
+    usage
+    exit 1
+}
+
+# get terminal inputs
+
+while getopts ":p:r:i:j:h:" flag
+do
+    case "${flag}" in
+        p) package_path=${OPTARG};;
+        r) export_root=${OPTARG};;
+        i) install_path=${OPTARG};;
+        j) cores=${OPTARG};;
+        s) installer=${OPTARG};;
+        h) usage;;
+    esac
+done
+
+if [[ $package_path != /* || $install_path != /* || $export_root != /* ]]; then
+    exit_abnormal "Please provide the absolute paths."    
+fi
+
+mkdir -p $package_path $install_path $export_root
+status=$?
+if test ! $status -eq 0; then
+    exit_abnormal "Could not create the necessary directories. please check the permissions."
+fi
+
+path_export="export PATH=\$PATH:$export_root/usr/bin:$export_root/usr/sbin"
+lib_export="export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$export_root/usr/lib:$export_root/usr/lib64"
+
+if ! grep -q "$path_export" $HOME/.bashrc; then
+    echo $path_export >> $HOME/.bashrc
+fi
+if ! grep -q "$lib_export" $HOME/.bashrc; then
+    echo $lib_export >> $HOME/.bashrc
+fi
+
+
+sed -i "s|^\s*package_path=.*|package_path=$package_path|g" $installer
+sed -i "s|^\s*install_path=.*|install_path=$install_path|g" $installer
+sed -i "s|^\s*export_root=.*|export_root=$export_root|g" $installer
+sed -i "s|^\s*cores=.*|cores=$cores|g" $installer
+
+source $HOME/.bashrc
---
+++ syminst.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# default values
+
+package_path=/home/bemghola/.apps/packages
+install_path=/home/bemghola/.apps/installs
+export_root=/home/bemghola/.apps/root
+cores=144
+
+
+ext="tar.gz"
+uninstall="false"
+
+# define functions
+
+usage() {                                 # Function: Print a help message.
+    echo "Usage: $0 [ -f filename ] [ -n name ] [ -e extension ] [ -l URL ] [ -u ] [ -p package_path ] [ -r export_root ] [ -i install_path ] [ -j cores ]" 1>&2 
+}
+
+exit_abnormal() {                         # Function: Exit with error.
+    echo "Error:" 1>&2
+    echo $1 1>&2
+    usage
+    exit 1
+}
+
+# get terminal inputs
+
+while getopts ":ul:f:e:p:r:i:j:h:" flag
+do
+    case "${flag}" in
+        u) uninstall="true";;
+        l) url=${OPTARG};;
+        n) name=${OPTARG};;
+        f) filename=${OPTARG};;
+        e) ext=${OPTARG};;
+        p) package_path=${OPTARG};;
+        r) export_root=${OPTARG};;
+        i) install_path=${OPTARG};;
+        j) cores=${OPTARG};;
+        h) usage;;
+    esac
+done
+
+# infere the name, filename, and url from each other if required and possible
+
+if [[ -z $name && -z $filename && ! -z $url ]]; then    # get filename from url
+    filename=`echo $url | rev | cut -d '/' -f1 | rev`
+fi
+if [[ -z $name && ! -z $filename ]]; then   # get name from filename
+    fext=${filename:${#filename}-${#ext}:${#filename}}
+    case $fext in 
+        tar.gz|tar.bz|tar.xz|$ext) 
+            ext=$fext;;
+        *) 
+            exit_abnormal "Unknown extension $fext of file $filenamme. Try setting the file extension using the flag -e";;
+    esac
+    name=${filename:0:${#filename}-${#ext}-1}
+elif [[ -z $filename && ! -z $name ]]; then     # get filename from name
+    filename="$name"."$ext"
+elif [[ -z $name && -z $filename ]]; then
+    exit_abnormal "Empty name and filename!"
+fi
+
+
+# perform install / uninstall
+
+if [ $uninstall == "false" ]; then
+
+    pushd $package_path
+    if [ ! -z "$url" ]; then
+        wget $url
+    fi
+
+    if [ ! -f $filenamme]; then
+        popd
+        exit_abnormal "Could not find file $filename under $package_path"
+    fi
+
+    tar -xf $filename
+    
+    if [ ! -d $name ]; then
+        popd
+        exit_abnormal "Could not find directory $name under $package_path"
+    fi
+
+    mkdir $install_path/$name
+    pushd $name
+
+    if [[ ! -f configure && -f autogen.sh ]]; then
+        ./autogen.sh
+        status=$?
+        if test ! $status -eq 0; then
+            popd
+            popd
+            exit_abnormal "Unsuccessful autogen.sh!"
+        fi
+    fi
+
+    ./configure --prefix=$export_root/usr
+    status=$?
+    if test ! $status -eq 0; then
+        popd
+        popd
+        exit_abnormal "Unsuccessful configure!"
+    fi
+
+    make -j$cores
+    status=$?
+    if test ! $status -eq 0; then
+        popd
+        popd
+        exit_abnormal "Unsuccessful make!"
+    fi
+
+    make DESTDIR=$install_path/$name install
+    status=$?
+    if test ! $status -eq 0; then
+        popd
+        popd
+        exit_abnormal "Unsuccessful make install!"
+    fi
+
+    cp -srv $install_path/"$name"$export_root/usr $export_root
+    popd
+    rm -rf $name
+    popd
+
+    echo "Package $name is symlink-installed under: $export_root"
+    echo "The regular installation files can be found under: $install_path/$name"
+
+elif [ $uninstall == "true" ]; then
+
+    find $export_root -lname "$install_path/$name*" -delete
+    rm -rf $install_path/$name
+#    pushd $package_path
+#    find $package_path -name $filename -delete
+#    popd
+    echo "Package $name has been removed!"
+
+fi
+
